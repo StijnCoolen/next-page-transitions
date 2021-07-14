3f82557fd2e3e78b761667c80e236ab93af0f2a0
Stijn Coolen <stijn@51north.nl>
2021-07-14 12:24:17 +0200

    Added basic transition

---
+++ components/Layout.js
@@ -0,0 +1,36 @@
+import {useEffect, useState} from 'react'
+import Link from 'next/link'
+import styles from "./Layout.module.css";
+
+export default function TransitionLayout({children}) {
+    const [displayChildren, setDisplayChildren] = useState(children);
+    const [transitionStage, setTransitionStage] = useState("fadeOut");
+
+    useEffect(() => {
+        setTransitionStage("fadeIn")
+    }, []);
+
+    useEffect(() => {
+        if (children !== displayChildren) setTransitionStage("fadeOut");
+      }, [children, displayChildren]);
+
+    return (
+        <div>
+            <nav>
+                <Link href="/">Home</Link>
+                <Link href="/about">About</Link>
+            </nav>
+            <div 
+                onTransitionEnd={() => {
+                    if (transitionStage === "fadeOut") {
+                        setDisplayChildren(children);
+                        setTransitionStage("fadeIn");
+                    }
+                }}
+                className={`${styles.content} ${styles[transitionStage]}`}
+            >
+                {displayChildren}
+            </div>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import TransitionLayout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <TransitionLayout>
+        <Component {...pageProps} />
+    </TransitionLayout>
+  )
 }
 
 export default MyApp
